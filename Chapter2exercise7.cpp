686527cba800bb693251116d29d801720e0e6f1b
Joshuathomas15 <jjulyo6mdls958@gmail.com>
2025-02-09 19:18:51 -0500

    Add project files.

---
+++ Chapter2exercise7.cpp
@@ -0,0 +1,14 @@
+// Chapter2exercise7.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+(
+	const double MIL_PER_YEAR=1.5;
+Cout; << "in 5 years the ocean level will be higher by" << MIL_PER_Year * 5 << "millimeters." << endl;
+Cout; << "in 7 years the ocean's level will be higher by" << MIL_PER_YEAR * 7 << "millimeters." << endl;
+cout; << "in 10 years the ocean's level will be higher by" << MIL_PER_YEAR * 10 << "millimeters." << endl;
+cout; << endl;
+
+return 0;
+	)
